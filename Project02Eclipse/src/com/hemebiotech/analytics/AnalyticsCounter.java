8fc0035df0b4cf27fc740df33779199683c122ee
olivier.bonheur <olivier.bonheur@gmail.com>
2020-03-06 11:40:02 +0100

    Use of a TreeMap<String, Integer> to count and sort symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,68 +1,56 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.Map.*;
+import java.io.*;
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int dialatedPupilsCount = 0;
 
 	public static void main(String args[]) {
 
-		BufferedReader reader=null;
-		FileWriter writer = null;
-		
 		try {
 
-			// first get and count input
-			reader = new BufferedReader(new FileReader(
+			// first get input
+			BufferedReader reader = new BufferedReader(new FileReader(
 					"C:\\Users\\olivi\\OneDrive\\Bureau\\OCR\\Projet2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt"));
 			String line = reader.readLine();
 
+			ArrayList<String> inputListSymptoms = new ArrayList<String>();
+
 			while (line != null) {
-				// System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headacheCount++;
-					// System.out.println("number of headaches: " + headacheCount);
-				} else if (line.equals("rash")) {
-					rashCount++;
-				} else if (line.equals("dialated pupils")) {
-					dialatedPupilsCount++;
-				}
+				inputListSymptoms.add(line);
 				line = reader.readLine();
 			}
 			reader.close();
 
-			// next generate output
-			writer = new FileWriter(
+			// then count and sort symptoms
+			TreeMap<String, Integer> tmCountSymptoms = new TreeMap<>();
+
+			ListIterator<String> itListSymptoms = inputListSymptoms.listIterator();
+			while (itListSymptoms.hasNext()) {
+				String eltListSymptoms = itListSymptoms.next();
+				tmCountSymptoms.put(eltListSymptoms,
+						(Integer) Collections.frequency(inputListSymptoms, eltListSymptoms));
+			}
+
+			// finally write results
+			FileWriter writer = new FileWriter(
 					"C:\\Users\\olivi\\OneDrive\\Bureau\\OCR\\Projet2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\result.out");
-			writer.write("headache: " + headacheCount + "\n");
-			writer.write("rash: " + rashCount + "\n");
-			writer.write("dialated pupils: " + dialatedPupilsCount + "\n");
+
+			Set<Entry<String, Integer>> setTm = tmCountSymptoms.entrySet();
+			Iterator<Entry<String, Integer>> itMap = setTm.iterator();
+			while (itMap.hasNext()) {
+				Entry<String, Integer> eltMap = itMap.next();
+				System.out.println(eltMap.getKey() + " : " + eltMap.getValue());
+
+				writer.write(eltMap.getKey() + " : " + eltMap.getValue() + "\n");
+
+			}
 			writer.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		finally { 
-            try { 
-            	if (reader!=null) 
-            		reader.close(); 
-            	} catch (Exception e) {
-            		e.printStackTrace();
-            	}
-            try {
-            	if (writer!=null) 
-            		writer.close(); 
-        		} catch (Exception e) {
-        			e.printStackTrace();
-        		}
-		}
-
-}
-
+	}
 }
